98738c647dba9727cdb3d03aa3085ec744199318
cedric duvillier <ced_duv@hotmail.com>
2021-10-23 11:15:22 +0200

    maj cour s3

--- 02_sql/querries/base_sql.rb
+++ 02_sql/querries/base_sql.rb
@@ -13,3 +13,13 @@ JOIN consultations ON doctors.id = consultations.doctor_id;
 #===
 SELECT * FROM doctors d
 JOIN consultations c ON d.id = c.doctor_id;
+
+#-------------------------------
+
+DB.results_as_hash = true
+
+doctors = DB.execute("SELECT name, age FROM doctors")
+# => [
+#      { "name" => "John Smith", "age" => 39 , 0 => "John Smith", 1 => 39 },
+#      { "name" => "Emma Reale", "age" => 31 , 0 => "Emma Reale", 1 => 31 }
+#    ]
