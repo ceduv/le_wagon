81d722a8277c72205c03b33a6fbf46509525f08a
cedric duvillier <ced_duv@hotmail.com>
2021-11-12 11:17:07 +0100

    first push

--- 10_rails/CRUD/movies_lists/db/schema.rb
+++ 10_rails/CRUD/movies_lists/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_02_19_155458) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bookmarks", force: :cascade do |t|
     t.text "comment"
     t.bigint "movie_id", null: false
