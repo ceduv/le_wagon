a403c93db5e8f0ba7b93937a9fc9c24dd71a2bd2
cedric duvillier <cedduvillier@gmail.com>
2021-11-15 21:48:55 +0100

    rails has many belongs to

--- 10_rails/has_many_belong_to.rb
+++ 10_rails/has_many_belong_to.rb
@@ -4,3 +4,12 @@
 # on passe a travers bookmark pour avoir acces a la table movies
 has_many :bookmarks
 has_many :movies, through: :bookmarks, dependent: :destroy
+
+class List < ApplicationRecord
+    has_many :bookmarks, dependent: :destroy
+    has_many :movies, through: :bookmarks
+    has_many :reviews, dependent: :destroy
+    has_one_attached :photo
+
+    validates :name, uniqueness: true, presence: true
+  end
